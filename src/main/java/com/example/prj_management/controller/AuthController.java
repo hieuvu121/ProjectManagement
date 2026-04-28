bee948b43798f3d21e0249f45a41d89f0e236ca0
hieuvu121 <hieubeobilabi@gmail.com>
2026-04-28 22:47:43 +1000

    final security config

---
+++ src/main/java/com/example/prj_management/controller/AuthController.java
@@ -0,0 +1,15 @@
+package com.example.prj_management.controller;
+
+import com.example.prj_management.security.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+}
