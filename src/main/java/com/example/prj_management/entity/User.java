961bcbd917b179fcf728aced44d737305204f6f4
hieuvu121 <hieubeobilabi@gmail.com>
2026-04-28 11:45:37 +1000

    feat: add User entity

---
+++ src/main/java/com/example/prj_management/entity/User.java
@@ -0,0 +1,27 @@
+package com.example.prj_management.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    @Column(nullable = false)
+    private String name;
+}
